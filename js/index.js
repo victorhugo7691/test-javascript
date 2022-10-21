7874f9a0aeb7faca731756eeac209e731f7c7acd
Victor Hugo <victor-f250@hotmail.com>
2022-10-21 02:22:50 -0300

    funcoes

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ let carro = {
     placa:"ABC-1232",
     buizina: function(){ alert("Biiiiiii")},
     completo: function(){ return "A marca e "+ this.marca }
-};//objeto literal, é determinado os valores deretamente
+};//objeto literal, é determinado os valores diretamente
 console.log(carro);
 //carro.buizina();
 
@@ -42,3 +42,89 @@ alteraACor = function(){
     let div = document.getElementById("div");
     div.style.backgroundColor = "red";
 };
+
+select = function(){
+    console.log("Mudou a opção")
+}
+
+//array
+
+const lista = ["arroz", "feijão", "carne", "macarrão"];
+const lista2 = ["Leite", "Suco", "Bolo", "Salgado", "Chá"];
+lista.push("Pamonha");
+//Remove o ultimo item lista.pop();
+//Remove o primeito item lista.shift();
+//Altera o item lista.unshift("Bonitao");
+//Deleta elemento da posição delete lista[0];
+//Adiciona novos itens lista.splice(1, 0, "Item adicionado 1", "Item adicionado 2");
+
+Array.isArray(lista);
+
+const superLista = lista.concat(lista2);
+console.log(superLista);
+
+imprimirALista = function(){
+    for(let i=0; i < lista.length; i++){
+        console.log(lista[i]);
+    }
+
+    console.log("");
+    lista.forEach(elemento => console.log(elemento));
+}
+
+//Fatiar o array
+const fatia = lista2.slice(1, 3);
+console.log(fatia);
+
+console.log(lista.sort()); //imprime em ordem alfabética
+//sort para numeros
+//numeros.sort(function(a, b){ return a-b });
+lista.reverse(); //inverte a ordem atual
+
+const numeros = [10, 20, 50, 9, 2];
+const maior20 = numeros.filter(filtragem);
+
+document.getElementById("vetor").innerHTML = lista.join(" - "); //separa cada elemento com -
+
+console.log(maior20);
+
+function filtragem(value, index, array){
+    return value > 20;
+}
+
+var horaAtual = new Date().getHours();
+document.getElementById("relogio").innerHTML = horaAtual;
+
+//tornar a letra minuscula
+// palavra.toLowerCase();
+
+for(let i = 0; i < 11; i++){
+    document.getElementById("texto2").innerHTML += i + ", ";
+}
+
+selecaoDeAno = function (){
+    var ano = new Date().getFullYear();
+
+    for(let i = ano; i >= 1996; i--){
+        document.getElementById("ano").innerHTML += "<option value='"+i+"'>"+i+"</option>";
+    }
+}
+
+var tempo = 0;
+
+apresentarTime = function(){
+    setTimeout(function(){
+        console.log("Executou timeout com 5000s");
+    },5000);
+}
+
+timerDeVisualizacao = function(){//Roda a cada 5000s
+    setInterval(function(){
+        tempo = tempo + 5000;
+        console.log("Visualização em "+ tempo);
+    }, 5000);
+}
+
+selecaoDeAno();
+timerDeVisualizacao();
+apresentarTime();
